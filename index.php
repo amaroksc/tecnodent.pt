d103081f74ff754f366b9198edc9c834428fbfd7
amaroksc <53928868+amaroksc@users.noreply.github.com>
2021-01-14 14:14:34 -0400

    tee

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>tecnodent</title>
 
 <!--FABICON-->
-<link rel="icon" href="favicon-tecnodent.png">
+<link rel="icon" href="image/favico">
 
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
